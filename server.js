e1711c005ec7c5584a36e292b820d6f001acf4d2
saraalramahi <saraalramahi2004@gmail.com>
2025-09-16 23:37:54 +0300

    flavor-table

--- server.js
+++ server.js
@@ -142,6 +142,6 @@ app.use("/api/users", userRoutes);
 
 
 // -------- Start server --------
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server listening on port http://localhost:${PORT}`));
 
